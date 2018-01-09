d1b3a87b320c535147b1aab92cadbc866da2d30d
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-09 14:31:57 +0300

    [Feature] Add SwitchStatus enum
    This enumeration will be changing switch status from on to off and vice versa.

---
+++ Eluxxon/Enums/SwitchStatus.swift
@@ -0,0 +1,14 @@
+//
+//  SwitchStatus.swift
+//  Eluxxon
+//
+//  Created by Ibrahim Mbaziira on 1/9/18.
+//  Copyright © 2018 Ibrahim Mbaziira. All rights reserved.
+//
+
+import Foundation
+
+enum SwitchStatus {
+    case on
+    case off
+}
