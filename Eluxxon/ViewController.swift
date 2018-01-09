2d2cef63c514d4878b96260c7522b775d20b2a8d
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-09 14:51:30 +0300

    [Feature] Add functionality for when the switch is off

--- Eluxxon/ViewController.swift
+++ Eluxxon/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var onOffLbl: UILabel!
-    @IBOutlet weak var toggleBtn: UILabel!
+    @IBOutlet weak var toggleBtn: UIButton!
+    var switchStatus: SwitchStatus = .off
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func toggleBtnWasPressed(_ sender: Any) {
+        switchStatus.toggle()
+        if switchStatus == .off {
+            toggleBtn.setImage(UIImage(named: "offBtn")!, for: .normal)
+            view.backgroundColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
+            onOffLbl.text = "🌒 OFF 🌒"
+            onOffLbl.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
     
 }
