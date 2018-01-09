b403783a691cac74be6f2f9a3de56323a5239b0f
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-09 14:25:27 +0300

    [Features] Application UI
    Build application UI and add assets.

--- Eluxxon/ViewController.swift
+++ Eluxxon/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onOffLbl: UILabel!
+    @IBOutlet weak var toggleBtn: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func toggleBtnWasPressed(_ sender: Any) {
     }
-
-
+    
 }
 
